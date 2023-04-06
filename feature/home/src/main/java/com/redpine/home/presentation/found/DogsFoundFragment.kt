2a78ab7540e931d93adf277f0fe4b1a34bfe86cb
@maiow <emmaria@yandex.ru>
2023-04-06 14:03:14 +0300

    After merge fixes.

--- feature/home/src/main/java/com/redpine/home/presentation/found/DogsFoundFragment.kt
+++ feature/home/src/main/java/com/redpine/home/presentation/found/DogsFoundFragment.kt
@@ -6,16 +6,16 @@ import android.view.View
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.hannesdorfmann.adapterdelegates4.ListDelegationAdapter
-import com.redpine.core.model.card.RecentSeenDog
+import com.redpine.core.model.card.Dog
 import com.redpine.home.HomeBaseFragment
 import com.redpine.home.databinding.FragmentDogsFoundBinding
-import com.redpine.home.presentation.home.delegate.dogListDelegate
+import com.redpine.home.presentation.home.delegate.dogsDelegate
 
 class DogsFoundFragment : HomeBaseFragment<FragmentDogsFoundBinding>() {
 
     override fun initBinding(inflater: LayoutInflater) = FragmentDogsFoundBinding.inflate(inflater)
     private val viewModel: DogsFoundViewModel by lazy { initViewModel() }
-    private val adapter by lazy { ListDelegationAdapter(dogListDelegate()) }
+    private val adapter by lazy { ListDelegationAdapter(dogsDelegate()) }
     private val args by navArgs<DogsFoundFragmentArgs>()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -33,7 +33,7 @@ class DogsFoundFragment : HomeBaseFragment<FragmentDogsFoundBinding>() {
         binding.searchParamsView.text = filters
     }
 
-    private fun loadContent(data: List<RecentSeenDog>) {
+    private fun loadContent(data: List<Dog>) {
         adapter.items = data
         binding.recyclerView.adapter = adapter
     }
--- feature/home/src/main/java/com/redpine/home/presentation/found/DogsFoundViewModel.kt
+++ feature/home/src/main/java/com/redpine/home/presentation/found/DogsFoundViewModel.kt
@@ -2,19 +2,19 @@ package com.redpine.home.presentation.found
 
 import androidx.lifecycle.ViewModel
 import com.redpine.api.Api
-import com.redpine.core.model.card.RecentSeenDog
+import com.redpine.core.model.card.Dog
 import kotlin.random.Random
 
 class DogsFoundViewModel(api: Api) : ViewModel() {
 
 
-    private val _foundDogList = mutableListOf<RecentSeenDog>()
+    private val _foundDogList = mutableListOf<Dog>()
 
     init {
         for (i in 1..10) {
             _foundDogList.add(
-                RecentSeenDog(
-                    i - 1, "number $i", "age ${i + 5} years", Random.nextBoolean(),
+                Dog(
+                    i - 1, "number $i", "age ${i + 5} years", "", Random.nextBoolean(),
                     Random.nextBoolean()
                 )
             )
