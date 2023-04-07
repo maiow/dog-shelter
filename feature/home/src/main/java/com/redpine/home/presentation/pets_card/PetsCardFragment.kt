e0b99f80b1aca3dbb874892c627604dd2e358599
@maiow <emmaria@yandex.ru>
2023-04-08 01:18:48 +0300

    Pets card screen added.

---
+++ feature/home/src/main/java/com/redpine/home/presentation/pets_card/PetsCardFragment.kt
@@ -0,0 +1,18 @@
+package com.redpine.home.presentation.pets_card
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import com.redpine.home.HomeBaseFragment
+import com.redpine.home.databinding.FragmentPetsCardBinding
+
+class PetsCardFragment : HomeBaseFragment<FragmentPetsCardBinding>() {
+
+    override fun initBinding(inflater: LayoutInflater) = FragmentPetsCardBinding.inflate(inflater)
+    private val viewModel: PetsCardViewModel by lazy { initViewModel() }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+}
---
+++ feature/home/src/main/java/com/redpine/home/presentation/pets_card/PetsCardViewModel.kt
@@ -0,0 +1,11 @@
+package com.redpine.home.presentation.pets_card
+
+import androidx.lifecycle.ViewModel
+import com.redpine.api.Api
+import javax.inject.Inject
+
+class PetsCardViewModel @Inject constructor(
+    private val api: Api,
+) : ViewModel() {
+    // TODO: Implement the ViewModel
+}
