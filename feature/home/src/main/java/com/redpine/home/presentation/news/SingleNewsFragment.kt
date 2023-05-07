6b2e8040f697786a6a62adb0ab703efcd20406c2
VSV <mr.vsv92@mail.ru>
2023-05-07 10:08:00 +0700

    добавил скролл и разбил текст по абзацам во фрагменте новости

--- feature/home/src/main/java/com/redpine/home/presentation/news/SingleNewsFragment.kt
+++ feature/home/src/main/java/com/redpine/home/presentation/news/SingleNewsFragment.kt
@@ -37,9 +37,14 @@ class SingleNewsFragment : HomeBaseFragment<FragmentSingleNewsBinding>() {
             viewModel.data.collect { newsResponse ->
                 newsResponse.news.let { news ->
                     if (news != null) {
+                        Log.d(TAG, "observeData: ${news.body}")
                         binding.newsPreview.loadImage(news.imageUrl)
                         binding.newsTitle.text = news.title
-                        binding.newsBody.text = news.body
+                        val body = news.body
+                            .replace("\\n", "\n")
+                            .replace("    ", "\n\n")
+                            .replace("•", "  •")
+                        binding.newsBody.text = body
                     }
                     //TODO: нужно добавить обработку newsResponse.exception
                 }
@@ -47,34 +52,35 @@ class SingleNewsFragment : HomeBaseFragment<FragmentSingleNewsBinding>() {
         }
     }
 
-        private fun infoLoadingObserve() {
-            viewLifecycleOwner.lifecycleScope.launch {
-                Log.d(TAG, "infoLoadingObserve: ")
-                viewModel.loadState.collect { loadState ->
-                    when (loadState) {
-                        LoadState.LOADING -> {
-                            binding.progressBar.isVisible = true
-                            binding.newsBody.isVisible = false
-                            binding.newsTitle.isVisible = false
-                        }
-
-                        LoadState.ERROR_NETWORK -> {
-                            binding.progressBar.isVisible = false
-                            binding.newsBody.isVisible = false
-                            binding.newsTitle.isVisible = false
-                            Toast.makeText(requireContext(), "loading error", Toast.LENGTH_SHORT)
-                                .show()
-                        }
-
-                        LoadState.SUCCESS -> {
-                            binding.progressBar.isVisible = false
-                            binding.newsBody.isVisible = true
-                            binding.newsTitle.isVisible = true
-                        }
-                        else->{}
-
+    private fun infoLoadingObserve() {
+        viewLifecycleOwner.lifecycleScope.launch {
+            Log.d(TAG, "infoLoadingObserve: ")
+            viewModel.loadState.collect { loadState ->
+                when (loadState) {
+                    LoadState.LOADING -> {
+                        binding.progressBar.isVisible = true
+                        binding.newsBody.isVisible = false
+                        binding.newsTitle.isVisible = false
                     }
+
+                    LoadState.ERROR_NETWORK -> {
+                        binding.progressBar.isVisible = false
+                        binding.newsBody.isVisible = false
+                        binding.newsTitle.isVisible = false
+                        Toast.makeText(requireContext(), "loading error", Toast.LENGTH_SHORT)
+                            .show()
+                    }
+
+                    LoadState.SUCCESS -> {
+                        binding.progressBar.isVisible = false
+                        binding.newsBody.isVisible = true
+                        binding.newsTitle.isVisible = true
+                    }
+
+                    else -> {}
+
                 }
             }
         }
     }
+}
