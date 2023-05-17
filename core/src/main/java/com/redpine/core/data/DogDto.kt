5e13004932cc71c94dcd126338ad4460e8ed2081
@maiow <emmaria@yandex.ru>
2023-05-18 01:22:38 +0300

    Изменила названия полей в DogDto в соответствии с Firebase db

--- core/src/main/java/com/redpine/core/data/DogDto.kt
+++ core/src/main/java/com/redpine/core/data/DogDto.kt
@@ -2,14 +2,12 @@ package com.redpine.core.data
 
 import com.google.firebase.database.Exclude
 import com.google.firebase.database.IgnoreExtraProperties
-import com.google.firebase.database.PropertyName
 
 @IgnoreExtraProperties
 class DogDto(
     var age: String = "",
     var character: String = "",
     var color: String = "",
-    @PropertyName("curator_phone")
     var curatorPhone: String = "",
     var gender: String = "",
     var height: String = "20",
@@ -18,7 +16,6 @@ class DogDto(
     var name: String = "",
     var size: String = "",
     var text: String = "",
-    @PropertyName("web_link")
     var webLink: String = "",
     @Exclude
     var isFavorite: Boolean = false,
