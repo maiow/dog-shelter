50b0b081da7aab1f0665b13b1f4b989f2d7bbdeb
@maiow <emmaria@yandex.ru>
2023-10-09 21:42:32 +0300

    Android 14 added; up dependencies versions + required fixes; dogs_found layout corrected.

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,13 +2,13 @@
 import com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
 
 plugins {
-    id("com.android.application") version "8.1.0" apply false
-    id("com.android.library") version "8.1.0" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.0" apply false
+    id("com.android.application") version "8.1.2" apply false
+    id("com.android.library") version "8.1.2" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.10" apply false
     id("com.google.gms.google-services") version "4.3.15" apply false
-    id("androidx.navigation.safeargs.kotlin") version "2.6.0" apply false
-    id("com.google.firebase.crashlytics") version "2.9.7" apply false
-    id("com.github.ben-manes.versions") version "0.47.0" apply false
+    id("androidx.navigation.safeargs.kotlin") version "2.7.4" apply false
+    id("com.google.firebase.crashlytics") version "2.9.9" apply false
+    id("com.github.ben-manes.versions") version "0.49.0" apply false
 }
 
 apply(plugin = "com.github.ben-manes.versions")
